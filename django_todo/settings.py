02d2c3ddc906a9366ab5cc82a5023c4c8e7ab156
Sarohia94 <sarohia94@gmail.com>
2023-05-01 19:21:32 +0000

    Try fix2

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-7w-oxyn2^k(!tw#+pwnv1!io$g49ci$gs#=&7plfyb5cn!+_w8')
+SECRET_KEY = 'django-insecure-7w-oxyn2^k(!tw#+pwnv1!io$g49ci$gs#=&7plfyb5cn!+_w8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['amrit-django-todo-app.herokuapp.com']
+ALLOWED_HOSTS = ['web: gunicorn django_todo.wsgi:application']
 
 
 # Application definition
