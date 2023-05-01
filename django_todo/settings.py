85fc9f7dd77c24d36851dcf7708b5aafad6ab182
Sarohia94 <sarohia94@gmail.com>
2023-05-01 19:32:02 +0000

    Try fix5

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '2erm2g25fi&()6+d)rp0(1gw!vp^v2aoxpmfe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['amrit-django-todo-app.herokuapp.com']
+ALLOWED_HOSTS = ['amrit-django-todo-app.herokuapp.com/']
 
 
 # Application definition
