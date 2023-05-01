efed59f5c64eb08e3c69182a11d06ee6cc724788
Sarohia94 <sarohia94@gmail.com>
2023-05-01 18:40:25 +0000

    Prepare to push to heroku

--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
-import os
+# import os
 
-if os.path.isfile("env.py"):
-    import env
+# if os.path.isfile("env.py"):
+#     import env
 
-os.environ.get("DATABASE_URL")
+# os.environ.get("DATABASE_URL")
 
-#  from pathlib import Path
-#  import os
-#  import dj_database_url
-#  import env
+ from pathlib import Path
+ import os
+ import dj_database_url
+ import env
 
-#  DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#  }
+ DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+ }
