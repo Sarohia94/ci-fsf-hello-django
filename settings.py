259a55ce361bef20b5f57432ad3932d6af9592a7
Sarohia94 <sarohia94@gmail.com>
2023-05-01 18:35:50 +0000

    Modify data and tetsing

---
+++ settings.py
@@ -0,0 +1,15 @@
+import os
+
+if os.path.isfile("env.py"):
+    import env
+
+os.environ.get("DATABASE_URL")
+
+#  from pathlib import Path
+#  import os
+#  import dj_database_url
+#  import env
+
+#  DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#  }
